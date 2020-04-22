6350b5b5505b1468a9a1ad6adb65c0ffeb3b11a5
Thea Ganden <wtp9998@hotmail.com>
2020-04-22 03:25:01 +0000

    Implement character counter for new tweets

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,21 @@
+//implementing the character counter into the DOM
+$(document).ready(function() {
+	// --- our code goes here ---
+	$(".new-tweet form textarea").keyup(function(event) {
+		let characterLength = $(this).val()
+		//go up the dom tree to get access to counter
+		let characterRemaining = $( this ).parent().find(".counter").html(140-characterLength.length)
+		//change the value of the character remaining into a string
+		let numOfCharacterRemaining = Number($(characterRemaining).val())
+		//if the number of characters remaing is negative turn the text red
+		if (numOfCharacterRemaining < 0) {
+			$(characterRemaining).css("color", "red");
+		//else maintain original color
+		} else {
+			$(characterRemaining).css("color", "#545149" )
+		}
+	})
+	
+	
+
+});
