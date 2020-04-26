3c8d071e0f9f0ec36136a906fdb5ae0c33b52caf
Thea Ganden <wtp9998@hotmail.com>
2020-04-26 22:04:16 +0000

    Add upload time using moment

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,99 +6,98 @@
 
 
 
- $(document).ready(function() {
+$(document).ready(function() {
 //const moment = require('moment')
 
 
-//render list of new tweets
-const renderTweets = function(tweets) {
-	const $tweets = $('.tweet-container')
-	$tweets.empty()
-	for (let tweet of tweets) {
-		$tweets.prepend(createTweetElement(tweet))
-	}
-	return $tweets
-}
+  //render list of new tweets
+  const renderTweets = function(tweets) {
+    const $tweets = $('.tweet-container');
+    $tweets.empty();
+    for (let tweet of tweets) {
+      $tweets.prepend(createTweetElement(tweet));
+    }
+    return $tweets;
+  };
 
-//recieve user info to produce tweet format
-const createTweetElement = function(tweet) {
-	const $tweet = $('<article>').addClass("tweet");
-	//header
-	const $tweetHeader = $('<div>').addClass("tweet-header");
-	const $tweetProfile = $('<div>').addClass("tweet-profile")
-	const $tweetPicture = $('<img>').attr('src', tweet.user.avatars)
-	const $tweetName = $('<div>').addClass("tweet-name").text(tweet.user.name);
-	const $tweetHandle = $("<div>").addClass("tweet-handle").text(tweet.user.handle)
-	//content
-	const $tweetContent = $('<p>').addClass("tweet-content").text(tweet.content.text)
-	const $line = $('<hr />')
-	//footer
-	const $footer = $('<footer>');
-	const $datePosted = $("<div>").addClass("date-posted").text(moment(tweet['created_at']).fromNow());
-	//$("<div>").addClass("date-posted").text(tweet.created_at); //moment
-	//create links
-	const $links = $('<div>').addClass("little-links")
-	const $flaglink = $('<i>').addClass("fas fa-flag")
-	const $heartlink = $('<i>').addClass("fas fa-heart")
-	const $retweetlink = $('<i>').addClass("fas fa-retweet")
-	//append header
-	$tweetProfile.append($tweetPicture).append($tweetName)
-	$tweetHeader.append($tweetProfile).append($tweetHandle)
-	//append links
-	$links.append($flaglink).append($heartlink).append($retweetlink)
-	//append footer
-	$footer.append($datePosted).append($links)
-	//append file
-	$tweet.append($tweetHeader).append($tweetContent).append($line).append($footer)
+  //recieve user info to produce tweet format
+  const createTweetElement = function(tweet) {
+    const $tweet = $('<article>').addClass("tweet");
+    //header
+    const $tweetHeader = $('<div>').addClass("tweet-header");
+    const $tweetProfile = $('<div>').addClass("tweet-profile");
+    const $tweetPicture = $('<img>').attr('src', tweet.user.avatars);
+    const $tweetName = $('<div>').addClass("tweet-name").text(tweet.user.name);
+    const $tweetHandle = $("<div>").addClass("tweet-handle").text(tweet.user.handle);
+    //content
+    const $tweetContent = $('<p>').addClass("tweet-content").text(tweet.content.text);
+    const $line = $('<hr />');
+    //footer
+    const $footer = $('<footer>');
+    const $datePosted = $("<div>").addClass("date-posted").text(moment(tweet['created_at']).fromNow());
+    //$("<div>").addClass("date-posted").text(tweet.created_at); //moment
+    //create links
+    const $links = $('<div>').addClass("little-links");
+    const $flaglink = $('<i>').addClass("fas fa-flag");
+    const $heartlink = $('<i>').addClass("fas fa-heart");
+    const $retweetlink = $('<i>').addClass("fas fa-retweet");
+    //append header
+    $tweetProfile.append($tweetPicture).append($tweetName);
+    $tweetHeader.append($tweetProfile).append($tweetHandle);
+    //append links
+    $links.append($flaglink).append($heartlink).append($retweetlink);
+    //append footer
+    $footer.append($datePosted).append($links);
+    //append file
+    $tweet.append($tweetHeader).append($tweetContent).append($line).append($footer);
 
-	return $tweet;
-}
+    return $tweet;
+  };
 
 
-//post to server using ajax to post tweet text to server
- $('form').submit(function(event) {
+  //post to server using ajax to post tweet text to server
+  $('form').submit(function(event) {
 
-	event.preventDefault();
-	const self = (this);
-	const characters = Number($(".counter").html())
-	const formData = $(this).serialize();
- 	//implement validation before sending text to server display warning if no text
-	if (formData === "text=" || formData === "text=null") {
-		$(".warning").slideUp("slow")
-		$(".warning").slideDown("slow").text("No text was entered!")
+    event.preventDefault();
+    const self = (this);
+    const characters = Number($(".counter").html());
+    const formData = $(this).serialize();
+    //implement validation before sending text to server display warning if no text
+    if (formData === "text=" || formData === "text=null") {
+      $(".warning").slideUp("slow");
+      $(".warning").slideDown("slow").text("No text was entered!");
 		
 
-		//check character length display warning if characters over 140
-	} else if (characters < 0) {
-		$(".warning").slideUp("slow")
-		$(".warning").slideDown("slow").text("Too many characters!")
+      //check character length display warning if characters over 140
+    } else if (characters < 0) {
+      $(".warning").slideUp("slow");
+      $(".warning").slideDown("slow").text("Too many characters!");
 		
-	} else {
-		$(".warning").slideUp("slow")
-		$.post('/tweets', formData)
-		.then(() => {
-			$(self)[0].reset();
-			loadTweets()
-		})
-		.catch(err => console.log(err))
-	}
- })
+    } else {
+      $(".warning").slideUp("slow");
+      $.post('/tweets', formData)
+        .then(() => {
+          $(self)[0].reset();
+          loadTweets();
+        })
+        .catch(err => console.log(err));
+    }
+  });
  
 
- //ajax get request to server to fetch tweet data
- const loadTweets = function () {
+  //ajax get request to server to fetch tweet data
+  const loadTweets = function() {
 	
-	 $.getJSON('/tweets')
-	 .then((tweets) => {
-		 //display the tweets from the data 
-		 renderTweets(tweets)
-		 
-	 })
-	}
+    $.getJSON('/tweets')
+      .then((tweets) => {
+        //display the tweets from the data
+        renderTweets(tweets);
+      });
+  };
 
 
-	loadTweets()
+  loadTweets();
 	
-})
+});
 
 
